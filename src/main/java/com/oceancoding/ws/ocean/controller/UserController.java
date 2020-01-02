1c3f94dbc1944f2f16dc73f1468890cb229a69c8
sheng.wang3 <sheng.wang3@nio.com>
2020-01-02 18:26:51 +0800

    接口修改

--- src/main/java/com/oceancoding/ws/ocean/controller/UserController.java
+++ src/main/java/com/oceancoding/ws/ocean/controller/UserController.java
@@ -2,17 +2,35 @@ package com.oceancoding.ws.ocean.controller;
 
 import com.oceancoding.ws.ocean.bean.OceanUser;
 import com.oceancoding.ws.ocean.exceptionHandler.BizException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.oceancoding.ws.ocean.exceptionHandler.GlobalExceptionHandler;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(tags = "用户管理相关接口")
+@RequestMapping("/user")
 public class UserController {
-    @PostMapping("/user")
-    public boolean insert(@RequestBody OceanUser user) {
-        System.out.println("开始新增...");
-        //如果姓名为空就手动抛出一个自定义的异常！
-        if(user==null){
-            throw  new BizException("-1","用户不能为空！");
-        }
+    private static final Logger logger =  LoggerFactory.getLogger(UserController.class);
+    @PostMapping("/")
+    @ApiOperation("用户注册的接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", defaultValue = "请输入用户名", required = true),
+            @ApiImplicitParam(name = "email", value = "邮箱", defaultValue = "请输入邮箱"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "form")
+    })
+    public boolean registerUser(@RequestParam(required = true)String username, String email, @RequestParam(required = true)String password) {
+        logger.info("用户开始注册");
+        //如果用户名为空就手动抛出一个自定义的异常！
+//        if(user.getUserName()==null){
+//            throw  new BizException("-1","用户不能为空！");
+//        }
+        
         return true;
     }
 
