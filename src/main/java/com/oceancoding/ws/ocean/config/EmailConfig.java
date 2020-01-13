b2e9fa37421d8e9953ad18419021627568e2d31a
sheng.wang3 <sheng.wang3@nio.com>
2020-01-13 13:25:38 +0800

    邮件服务

---
+++ src/main/java/com/oceancoding/ws/ocean/config/EmailConfig.java
@@ -0,0 +1,60 @@
+package com.oceancoding.ws.ocean.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfig {
+    private String host;
+    private Integer port;
+    private String username;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    @Bean
+    public MailSender javaMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        return javaMailSender;
+    }
+}
